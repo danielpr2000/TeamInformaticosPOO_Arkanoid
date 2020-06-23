0e323d010ee81a201825309905e7a2eb4bb0cefe
danielpr2000 <00365918@uca.edu.sv>
2020-06-23 01:31:23 -0600

    ad

---
+++ Arkanoid/Arkanoid/Controlador/PlayerData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Arkanoid
+{
+    public static class PlayerData
+    {
+        public static bool CreatePlayer(string nickname)
+        {
+            var dt = ConnectionDB.ExecuteQuery($"SELECT * FROM PLAYER WHERE nickname = '{nickname}'");
+
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                ConnectionDB.ExecuteNonQuery($"INSERT INTO PLAYER(nickname) VALUES ('{nickname}')");
+
+                return false;
+            }
+        }
+
+        public static void NewScore(string idPlayer, int sc)
+        {
+            ConnectionDB.ExecuteNonQuery("INSERT INTO  SCORE(IdPlayer, score) VALUES ({idPlayer},{score})");
+        }
+
+        public static List<Player> PlayersTop()
+        {
+            var topPlayers = new List<Player>();
+            var dt = ConnectionDB.ExecuteQuery("SELECT pl.nickname, sc score FROM PLAYER pl, SCORE sc" +
+                                               "WHERE pl.IdPlayer = sc.IdPlayer ORDER BY sc.score DESC LIMIT 10");
+
+            foreach (DataRow dr  in dt.Rows)
+            {
+                topPlayers.Add(new Player(dr[0].ToString(),Convert.ToInt32(dr[1])));
+            }
+            
+            return topPlayers;
+        }
+    }
+}
---
+++ Arkanoid/Arkanoid/Vista/Top10.Designer.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+
+namespace Arkanoid
+{
+    partial class Top10
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Top10));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Showcard Gothic", 26.25F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl1.ForeColor = System.Drawing.Color.White;
+            this.lbl1.Location = new System.Drawing.Point(283, -1);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(625, 113);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Top 10";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 681);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(472, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Top10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (29)))), ((int) (((byte) (126)))),
+                ((int) (((byte) (222)))));
+            this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Top10";
+            this.Size = new System.Drawing.Size(1175, 822);
+            this.Load += new System.EventHandler(this.Top10_Load);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
